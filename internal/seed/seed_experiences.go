9049376a0a852e358ce2f9765f9b584e7b995250
wecrazy <wegirandolhistaralittu@gmail.com>
2026-03-08 17:01:09 +0700

    add: IT internship in experiences

--- internal/seed/seed_experiences.go
+++ internal/seed/seed_experiences.go
@@ -14,6 +14,8 @@ func seedExperiences(db *gorm.DB) {
 	workEnd := time.Date(2026, time.April, 3, 0, 0, 0, 0, time.UTC)
 	eduStart := time.Date(2018, time.January, 1, 0, 0, 0, 0, time.UTC)
 	eduEnd := time.Date(2022, time.September, 1, 0, 0, 0, 0, time.UTC)
+	internStart := time.Date(2021, time.January, 1, 0, 0, 0, 0, time.UTC)
+	internEnd := time.Date(2021, time.February, 28, 0, 0, 0, 0, time.UTC)
 
 	experiences := []model.Experience{
 		{
@@ -29,6 +31,19 @@ func seedExperiences(db *gorm.DB) {
 			ImageURL:    "https://image-service-cdn.seek.com.au/ca1202d0d39bddb50cefc1b6b7a050c435669c2e/",
 		},
 		{
+			Type:        "Work",
+			Title:       "IT Internship",
+			Org:         "Department of Agriculture and Plantation, Enrekang Regency",
+			Location:    "Enrekang Regency, South Sulawesi, Indonesia",
+			StartDate:   internStart,
+			EndDate:     &internEnd,
+			IsCurrent:   false,
+			Description: "Participated in a university internship program at the Department of Agriculture and Plantation of Enrekang, assisting with distribution reporting for fertilizer distribution and recipients of agricultural materials and equipment. Contributed to developing a simple informational website and participated in technology outreach activities, introducing practical uses of technology for the department and local communities, particularly in the agriculture and plantation sectors.",
+			SortOrder:   2,
+			ImageURL:    "https://lh3.googleusercontent.com/gps-cs-s/AHVAwer1Kwym4neteWEvT0CoKF61AqkAY1Bb9wIctWsreBzON1j4gr0Z7Lr933heoZMis8WJUNxEEP8G0uOc4LzpAUy8EQyjFF1DGLrrQXseLkIH81mArM6uiQJ-Y4ZmdlnbEYD2GGA=s680-w680-h510-rw",
+		},
+
+		{
 			Type:        "Education",
 			Title:       "Bachelor of Informatics Engineering",
 			Org:         "Universitas Kristen Indonesia Toraja",
@@ -37,7 +52,7 @@ func seedExperiences(db *gorm.DB) {
 			EndDate:     &eduEnd,
 			IsCurrent:   false,
 			Description: "Graduated Cum Laude with a GPA of 3.89, earning a Bachelor’s degree in Informatics Engineering (S.Kom) from the Faculty of Engineering, Universitas Kristen Indonesia Toraja in 2022, and was recognized as the best graduate of the program.",
-			SortOrder:   2,
+			SortOrder:   3,
 			CertURL:     "https://drive.google.com/file/d/1A0hDQingopqunPV87WMY93O3JOWjg92I/view?usp=sharing",
 			ImageURL:    "https://ukitoraja.ac.id/wp-content/uploads/2019/05/Logo-UKIT.png",
 		},
